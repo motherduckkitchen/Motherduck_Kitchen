c5c302659c901f0d3cb2b97fba5b08e85aafb9b7
motherduckkitchen <adamnorris80@gmail.com>
2026-03-14 13:15:52 +1000

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,9 @@
 window.MDK_FIREBASE_CONFIG = {
-  apiKey: "PASTE_NEW_FIREBASE_API_KEY_HERE",
-  authDomain: "PASTE_NEW_PROJECT.firebaseapp.com",
-  projectId: "PASTE_NEW_PROJECT_ID",
-  storageBucket: "PASTE_NEW_PROJECT.firebasestorage.app",
-  messagingSenderId: "PASTE_NEW_SENDER_ID",
-  appId: "PASTE_NEW_APP_ID",
-  measurementId: "PASTE_NEW_MEASUREMENT_ID"
+  apiKey: "AIzaSyBRTV9KfMyjCGk1XYnsCoOBZaZ9h-sU6UA",
+  authDomain: "motherduckkitchen-e2e1c.firebaseapp.com",
+  projectId: "motherduckkitchen-e2e1c",
+  storageBucket: "motherduckkitchen-e2e1c.firebasestorage.app",
+  messagingSenderId: "459081978963",
+  appId: "1:459081978963:web:8ac93c105b4a86c504a2a6",
+  measurementId: "G-F1933BLB5H"
 };
