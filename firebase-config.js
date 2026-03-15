d7387d48485db03d2bdc5bbc6cb698323f115e44
motherduckkitchen <adamnorris80@gmail.com>
2026-03-15 18:59:53 +1000

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+window.MDK_FIREBASE_CONFIG = {
+  apiKey: "YOURKEY",
+  authDomain: "motherduckkitchen.firebaseapp.com",
+  projectId: "motherduckkitchen",
+  storageBucket: "motherduckkitchen.appspot.com",
+  messagingSenderId: "XXXXXXXX",
+  appId: "XXXXXXXX"
+};
