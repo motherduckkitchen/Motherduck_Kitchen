99f11dab752f567f0bcaffa2360372fcbdc0ea22
motherduckkitchen <adamnorris80@gmail.com>
2026-03-15 20:05:42 +1000

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,9 @@
 window.MDK_FIREBASE_CONFIG = {
-  apiKey: "YOURKEY",
-  authDomain: "motherduckkitchen.firebaseapp.com",
-  projectId: "motherduckkitchen",
-  storageBucket: "motherduckkitchen.appspot.com",
-  messagingSenderId: "XXXXXXXX",
-  appId: "XXXXXXXX"
+  apiKey: "AIzaSyBRTV9KfMYjCGk1XYnsCoOBZaZ9h-sU6UA",
+  authDomain: "motherduckkitchen-e2e1c.firebaseapp.com",
+  projectId: "motherduckkitchen-e2e1c",
+  storageBucket: "motherduckkitchen-e2e1c.firebasestorage.app",
+  messagingSenderId: "459081978963",
+  appId: "1:459081978963:web:8ac93c105b4a86c504a2a6",
+  measurementId: "G-F1933BLB5H"
 };
